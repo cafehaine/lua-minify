831c45e09705213777def763e709820aec663fb7
cafehaine <kilian.guillaume@gmail.com>
2019-06-15 12:01:37 +0200

    tests: add tests for html

---
+++ tests/html.lua
@@ -0,0 +1,32 @@
+local html = require("minify.html")
+
+------------------------------------------------------------
+-- Check that we can at least run minify on basic strings --
+------------------------------------------------------------
+
+assert(html.minify("") == "", "html.minify didn't return an empty string.")
+
+assert(
+	html.minify("<!DOCTYPE html>") == "<!DOCTYPE html>",
+	"html.minify stripped the doctype."
+)
+
+assert(
+	html.minify("<!-- This is a comment -->") == "",
+	"html.minify didn't strip a comment."
+)
+
+assert(
+	html.minify("<html> <head> </head> </html>") == "<html><head></head></html>",
+	"html.minify left some useless spaces."
+)
+
+assert(
+	html.minify("<pre>  spaces ! </pre>") == "<pre>  spaces ! </pre>",
+	"html.minify stripped some spaces from a <pre> tag."
+)
+
+assert(
+	html.minify("<p> spaces  ?</p>") == "<p>spaces ?</p>",
+	"html.minify left some useless spaces."
+)
--- tests/main.lua
+++ tests/main.lua
@@ -1,6 +1,6 @@
 package.path = "../?.lua;../?/init.lua"
 
-local tests = {"striter"}
+local tests = {"striter","html"}
 
 for i=1, #tests do
 	local test = tests[i]
