4898be0fdd944f878fb55bb7bd3c28790dfe3f68
cafehaine <kilian.guillaume@gmail.com>
2019-06-22 16:28:08 +0200

    common: split parse_string in two functions

--- minify/common.lua
+++ minify/common.lua
@@ -1,29 +1,28 @@
 local m = {}
 
-local function continue_string(char, opening, no_escaping, ignore)
-	if not char then
-		return false
-	end
-	if char == opening then
-		if no_escaping then
-			return false
-		elseif ignore then
-			return true
-		end
-		return false
-	end
-	return true
-end
-
-function m.parse_string(iter, no_escaping)
+function m.parse_string_no_escape(iter)
 	local opening_char = iter:next()
 	local output = {opening_char}
-	local ignore_next = false
 	local char = iter:next()
-	while continue_string(char, opening_char, no_escaping, ignore_next) do
-		ignore_next = false
-		if char == "\\" and not no_escaping then
-			ignore_next = true
+	while char ~= opening_char do
+		output[#output+1] = char
+		char=iter:next()
+	end
+	output[#output+1] = char
+	return table.concat(output)
+end
+
+function m.parse_string(iter)
+	local opening_char = iter:next()
+	local output = {opening_char}
+	local ignore = false
+	local prev_ignore = ignore
+	local char = iter:next()
+	while char and not (char == opening_char and not ignore) do
+		prev_ignore = ignore
+		ignore = false
+		if char == "\\" and not prev_ignore then
+			ignore = true
 		end
 		output[#output+1] = char
 		char = iter:next()
--- tests/common.lua
+++ tests/common.lua
@@ -1,19 +1,27 @@
 local striter = require("minify.__striter")
 local common = require("minify.common")
-local parse_str = common.parse_string
+local parse_str_esc = common.parse_string
+local parse_str = common.parse_string_no_escape
 local iter = striter.new
 
 compare(parse_str(iter([["hello"]])), [["hello"]])
 compare(parse_str(iter([['hello']])), [['hello']])
+compare(parse_str_esc(iter([["hello"]])), [["hello"]])
+compare(parse_str_esc(iter([['hello']])), [['hello']])
 
-compare(parse_str(iter([["hello\"a"]]), true), [["hello\"]],
-	"failed with no_escaping = true")
-compare(parse_str(iter([['hello\'a']]), true), [['hello\']],
-	"failed with no_escaping = true")
+compare(parse_str(iter([["\\""]])), [["\\"]])
+compare(parse_str(iter([["\\\""]])), [["\\\"]])
+compare(parse_str_esc(iter([["\\""]])), [["\\"]])
+compare(parse_str_esc(iter([["\\\""]])), [["\\\""]])
 
-compare(parse_str(iter([["hello\"a"]]), false), [["hello\"a"]],
-	"failed with no_escaping = false")
-compare(parse_str(iter([['hello\'a']]), false), [['hello\'a']],
-	"failed with no_escaping = false")
+compare(parse_str(iter([["hello\"a"]])), [["hello\"]],
+	"failed with no escaping")
+compare(parse_str(iter([['hello\'a']])), [['hello\']],
+	"failed with no escaping")
+
+compare(parse_str_esc(iter([["hello\"a"]])), [["hello\"a"]],
+	"failed with escaping")
+compare(parse_str_esc(iter([['hello\'a']])), [['hello\'a']],
+	"failed with escaping")
 
 compare(parse_str(iter([["'"]])), [["'"]])
